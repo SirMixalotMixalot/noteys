4a38da2a0b3555666a6fe99e4622640f3a16f5c4
Bk <Bigbk75@gmail.com>
2022-04-09 16:33:09 +0200

    Added caching to NoteService

--- lib/services/crud/notes.dart
+++ lib/services/crud/notes.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+import 'dart:developer';
+
 import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:sqflite/sqflite.dart';
@@ -9,6 +12,9 @@ import 'db_user.dart';
 
 class NotesService {
   Database? _db;
+
+  List<DBNote> _notes = [];
+  final _noteStreamController = StreamController<List<DBNote>>.broadcast();
   Future<List<Map<String, Object?>>> queryUser(String email) async {
     final db = _getDataBaseOrThrow();
     final results = await db.query(
@@ -20,6 +26,11 @@ class NotesService {
     return results;
   }
 
+  Future _cacheNotes() async {
+    final notes = await getAllNotes();
+    _updatesNotes(() => _notes = notes.toList());
+  }
+
   Future<DBNote> updateNote({
     required DBNote note,
     required String text,
@@ -28,19 +39,36 @@ class NotesService {
 
     await getNote(id: note.id);
 
-    final updatesCount = await db.update(notesTable, {
-      textColumn: text,
-      isSyncedColumn: 0,
-    });
+    final updatesCount = await db.update(
+      notesTable,
+      {
+        textColumn: text,
+        isSyncedColumn: 0,
+      },
+      where: '$idColumn = ?',
+      whereArgs: [note.id],
+    );
     if (updatesCount == 0) {
       throw CouldNotUpdateNote();
     } else {
-      return DBNote(
+      final updatedNote = DBNote(
         id: note.id,
         userId: note.userId,
         text: text,
         isSynchedWithCloud: false,
       );
+      final index = _notes.indexWhere((element) => element.id == note.id);
+      if (index < 0) {
+        //should never happen since we  are updating a note
+        //but in the off chance it does occur
+        log("Adding an updated note to the cache ???");
+        _updatesNotes(() => _notes.add(updatedNote));
+      } else {
+        _updatesNotes(() {
+          _notes[index] = updatedNote;
+        });
+      }
+      return updatedNote;
     }
   }
 
@@ -59,19 +87,45 @@ class NotesService {
     return results.map(DBNote.fromRow);
   }
 
+  void _updatesNotes(Function mutateNotes) {
+    mutateNotes();
+    _noteStreamController.add(_notes);
+  }
+
   Future<DBNote> getNote({required int id}) async {
+    //notes could be updated and no good way to check if a
+    //note was updated without going to the database
+/*     final matchingNotes = _notes.where((note) => note.id == id);
+    final noteCached = matchingNotes.isNotEmpty;
+    if (noteCached) {
+      return matchingNotes.first;
+    } */
     final db = _getDataBaseOrThrow();
-    final results =
-        await db.query(notesTable, where: '$idColumn = ?', whereArgs: [id]);
+    final results = await db.query(
+      notesTable,
+      where: '$idColumn = ?',
+      whereArgs: [id],
+    );
     if (results.isEmpty) {
       throw CouldNotFindNote();
     } else {
-      return DBNote.fromRow(results.first);
+      final note = DBNote.fromRow(results.first);
+
+      _updatesNotes(() {
+        final index = _notes.indexWhere((n) => n.id == id);
+        if (index < 0) {
+          _notes.add(note);
+        } else {
+          _notes[index] = note;
+        }
+      });
+      return note;
     }
   }
 
   Future<int> deleteAllNotes() async {
     final db = _getDataBaseOrThrow();
+    _updatesNotes(() => _notes = []);
     return await db.delete(notesTable);
   }
 
@@ -84,6 +138,10 @@ class NotesService {
     );
     if (deletedCount == 0) {
       throw CouldNotDeleteNote();
+    } else {
+      _updatesNotes(
+        () => _notes.removeWhere((note) => note.id == id),
+      );
     }
   }
 
@@ -99,12 +157,14 @@ class NotesService {
       textColumn: text,
       isSyncedColumn: 1,
     });
-    return DBNote(
+    final note = DBNote(
       id: id,
       userId: owner.id,
       text: text,
       isSynchedWithCloud: true,
     );
+    _updatesNotes(() => _notes.add(note));
+    return note;
   }
 
   Future<DBUser> getUser({required String email}) async {
@@ -114,6 +174,16 @@ class NotesService {
       throw UserDoesNotExist();
     } else {
       return DBUser.fromRow(results.first);
+    }
+  }
+
+  Future<DBUser> getOrCreateUser({required String email}) async {
+    try {
+      return await getUser(email: email);
+    } on UserDoesNotExist {
+      return await createUser(email: email);
+    } catch (e) {
+      rethrow;
     }
   }
 
@@ -153,6 +223,7 @@ class NotesService {
       await _db?.execute(createUserTable);
 
       await _db?.execute(createNotesTable);
+      await _cacheNotes();
     } on MissingPlatformDirectoryException {
       throw UnableToGetDocumentDirectory();
     }
